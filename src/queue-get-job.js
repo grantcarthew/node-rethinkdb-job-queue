610fb89101148ec7b90008a62d5cca699b0b51b8
Grant Carthew <grant@carthew.net>
2016-06-15 12:14:55 +1000

    removed duplicate get next job

--- src/queue-get-job.js
+++ src/queue-get-job.js
@@ -1,6 +1,4 @@
 const logger = require('./logger')(module)
-const moment = require('moment')
-const enums = require('./enums')
 const dbResult = require('./db-result')
 
 module.exports = function (q, jobId) {
@@ -14,29 +12,5 @@ module.exports = function (q, jobId) {
     .run()
     .then((jobsData) => {
       return dbResult.toJob(q, jobsData)
-    })
-}
-
-module.exports.next = function (q) {
-  logger('getNextJob')
-  logger(`Concurrency: ${q.concurrency} Running: ${q.running}`)
-  const quantity = q.concurrency - q.running
-  return q.r
-    .table(q.name)
-    .orderBy({index: enums.index.inactive_priority_dateCreated})
-    .limit(quantity)
-    .update({
-      status: enums.jobStatus.active,
-      dateStarted: q.r.now()
-    }, {returnChanges: true})
-    .default({})
-    .run().then((updateResult) => {
-      console.dir(updateResult)
-      if (updateResult.changes) {
-        return updateResult.changes.map((change) => {
-          return q.createJob(null, change.new_val)
-        })
-      }
-      return []
     })
 }
