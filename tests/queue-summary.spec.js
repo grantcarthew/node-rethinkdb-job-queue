c2ebf9be4325f28a1abf92ac147b19d36a8fb285
Grant Carthew <grant@carthew.net>
2016-11-04 10:23:10 +1000

    Fixed small bug in tests

--- tests/queue-summary.spec.js
+++ tests/queue-summary.spec.js
@@ -15,19 +15,19 @@ module.exports = function () {
 
       const q = new Queue(tOpts.cxn(), tOpts.default())
       let jobs = []
-      for (let i = 0; i < 7; i++) {
+      for (let i = 0; i < 6; i++) {
         jobs.push(q.createJob())
       }
       jobs[0].status = enums.status.waiting
       jobs[1].status = enums.status.active
       jobs[2].status = enums.status.completed
       jobs[3].status = enums.status.cancelled
-      jobs[5].status = enums.status.failed
-      jobs[6].status = enums.status.terminated
+      jobs[4].status = enums.status.failed
+      jobs[5].status = enums.status.terminated
 
       return q.reset().then((resetResult) => {
         t.ok(is.integer(resetResult), 'Queue reset')
-        return queueAddJob(q, jobs, true)
+        return queueAddJob(q, jobs)
       }).then(() => {
         return queueSummary(q)
       }).then((summary) => {
@@ -37,7 +37,7 @@ module.exports = function () {
         t.equal(summary.cancelled, 1, 'Queue status summary includes cancelled')
         t.equal(summary.failed, 1, 'Queue status summary includes failed')
         t.equal(summary.terminated, 1, 'Queue status summary includes terminated')
-        t.equal(summary.total, 7, 'Queue status summary includes total')
+        t.equal(summary.total, 6, 'Queue status summary includes total')
         return q.reset()
       }).then((resetResult) => {
         t.ok(resetResult >= 0, 'Queue reset')
