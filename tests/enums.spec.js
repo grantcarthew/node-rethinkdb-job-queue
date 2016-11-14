772b3216d6a4701ed0d279c45e14b9b00758738b
Grant Carthew <grant@carthew.net>
2016-11-14 14:01:14 +1000

    Inc index test

--- tests/enums.spec.js
+++ tests/enums.spec.js
@@ -17,7 +17,7 @@ module.exports = function () {
       t.equal(Object.keys(enums.priority).length, 6, 'Enums priority has correct number of keys')
       t.equal(Object.keys(enums.status).length, 26, 'Enums status has correct number of keys')
       t.equal(Object.keys(enums.options).length, 13, 'Enums options has correct number of keys')
-      t.equal(Object.keys(enums.index).length, 3, 'Enums index has correct number of keys')
+      t.equal(Object.keys(enums.index).length, 4, 'Enums index has correct number of keys')
       t.equal(Object.keys(enums.log).length, 3, 'Enums log has correct number of keys')
       t.equal(Object.keys(enums.message).length, 25, 'Enums message has correct number of keys')
     } catch (err) {
